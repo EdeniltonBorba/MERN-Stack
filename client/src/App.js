d1b126e5bf48e641940641ce9382ed9f682ccf6e
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-23 18:05:41 +0200

    folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Board from './pages/client/board/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <h1>MERN-Stack</h1>
-      </header>
+  </header>*/}
+      <Board />
     </div>
   );
 }
