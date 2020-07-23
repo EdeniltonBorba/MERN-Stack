40f2e6f5b79b9b02eedf4bf24293a84a24094337
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-23 18:20:46 +0200

    folder structure

---
+++ client/src/pages/admin/product/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProductListing() {
+    return (
+        <div>
+            <h1>ProductListing</h1>
+        </div>
+    );
+}
+
---
+++ client/src/pages/admin/product/register.product.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RegisterProduct() {
+    return (
+        <div>
+            <h1>Register Product</h1>
+        </div>
+    );
+}
