ec976dddca6167288501bbeb3c66278a0d9ff959
Edenilton <edeniltonborba.adm@gmail.com>
2020-07-24 19:21:10 +0200

    add deshboard

--- client/src/components/menu-admin.js
+++ client/src/components/menu-admin.js
@@ -11,6 +11,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const drawerWidth = 240;
 
@@ -91,6 +92,7 @@ export default function MenuAdmin() {
 
     return (
         <>
+            <CssBaseline />
             <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
                 <Toolbar className={classes.toolbar}>
                     <IconButton
